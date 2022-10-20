f76e7168db5c7a3455954f5d322fe52470b5e658
Rohit sharma <rrohitsharma05@gmail.com>
2022-10-20 09:01:04 +0530

    port updated

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.get("*", (req, res) => {
 });
 
 // Start Server
-app.listen(3000, () => {
-  console.log(`Server Started at ${3000}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server Started at ${port}`);
 });
